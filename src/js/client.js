78d79f26cfc8e94c94d35ace83cf89b6d6d28fbe
Mirko Bellabarba <mirko.bellabarba@gmail.com>
2017-01-07 13:29:51 +0100

    Test

--- src/js/client.js
+++ src/js/client.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Layout extends React.Component {
-    render(){
-        console.log('hello world');
-        return (
-            <h1>imPulse</h1>
-        );
-    }
-}
+import Layout from './components/Layout';
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
---
+++ src/js/components/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class Layout extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
